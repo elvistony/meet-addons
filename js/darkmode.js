7419fc0685cf7d25841e9e5639a4681d41cd54c9
Elvis Tony <elvistonyrency@gmail.com>
2021-04-19 11:06:45 +0530

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,43 +1,65 @@
-function googleMeetDark(){
-document.body.style.transition="1s ease";
-if(!document.querySelector('[aria-label="Leave call"]')){
-    console.log("Not Joined Call!");
-    var status=document.querySelector('[aria-label="Leave call"]');
-    var listening = setInterval(()=>{
-        if(status){
-            setTimeout(()=>{DoMagic();},2000);
-            clearInterval(listening);
-        }else{
-            status=document.querySelector('[aria-label="Leave call"]');
-            console.log("Waiting to Join Call");
-        }
-    },3000)
-}else{
-    console.log("Joined Call");
-    setTimeout(()=>{DoMagic();},2000);
-}
+function googleMeetDark() {
 
-function DoMagic(){
-    var mic = document.querySelector('[data-is-muted]');
-    var parent = mic.parentElement.parentElement.parentElement.parentElement.parentElement;
-    var bottom = parent.children[8];
-    bottom.style.background='transparent';
-    bottom.children[2].children[1].style.display='none';
-    //bottom.children[2].children[2].style.display='none';
+    if (!document.querySelector('[aria-label="Leave call"]')) {
+        console.log("Not Joined Call!");
+        var status = document.querySelector('[aria-label="Leave call"]');
+        var listening = setInterval(() => {
+            if (status) {
+                setTimeout(() => { ThemeMyMeet(); }, 2000);
+                clearInterval(listening);
+            } else {
+                // Get Audio/Video buttons before joining call and switch them off.
+                camera = document.querySelectorAll('[data-tooltip="Turn off camera (ctrl + e)"]');
+                mics = document.querySelectorAll('[data-tooltip="Turn off microphone (ctrl + d)"]');
+                if (camera[0].length > 0) camera[0].click();
+                if (mics[0].length > 0) mics[0].click();
+                status = document.querySelector('[aria-label="Leave call"]');
+                console.log("Waiting to Join Call");
+            }
+        }, 3000)
+    } else {
+        console.log("Joined Call");
+        setTimeout(() => { ThemeMyMeet(); }, 2000);
+    }
 
-    var sidebar = parent.children[2]
-    sidebar.style.filter="invert(0.9)";
+    function ThemeMyMeet() {
+        var mic = document.querySelector('[data-is-muted]');
+        var parent = mic.parentElement.parentElement.parentElement.parentElement.parentElement;
+        var bottom = parent.children[8];
 
-    var rightnotif=parent.children[10];
-    rightnotif.style.opacity=0.4;
-    rightnotif.style.filter="invert(0.9)";
-    
-    parent.children[5].children[2].children[0].children[1].children[4].style.display='none'
-    parent.children[5].children[2].children[0].children[1].children[0].style.display='none';
-    parent.children[5].style.opacity='0.7';
-    parent.children[5].children[2].children[0].children[1].children[6].style.display="none";
-    parent.children[5].children[2].children[0].children[1].style.opacity="0.5";
-    console.log("Successfully Customized!")
-}
+
+        bottom.style.background = 'transparent';
+        bottom.style.boxShadow ="none";
+        bottom.children[2].children[1].style.display = 'none';
+
+        var sidebar = parent.children[2]
+        sidebar.style.filter = "invert(0.9)";
+
+        var rightnotif = parent.children[10];
+        rightnotif.style.opacity = 0.4;
+        rightnotif.style.background = "transparent";
+
+        parent.children[5].style.opacity = '0.7';
+
+        // Hide Presenting Now Name
+        parent.children[0].children[1].children[1].children[0].children[1].style.display = "none";
+
+        // Member Advertisement
+        parent.children[0].children[2].children[0].children[0].style.display = "none"
+
+        //Show Members Button Top Right
+        parent.children[0].children[2].children[0].children[1].children[0].style.display = "none";
+
+        //Hide Raise Hand Button
+        parent.children[8].children[2].children[0].style.display = "none";
+
+        //Remove ChatBox Box Shadow
+        parent.children[3].style.boxShadow = "none";
+
+        //Invert sidebar colors
+        parent.children[3].style.filter = "invert(0.93)";
+
+        console.log("Successfully Customized!");
+    }
 }
 googleMeetDark()
